76e380d5b6a47b530b1b0404cf65413caca81cc6
xiangjl <xjl-tommy@qq.com>
2017-08-14 12:55:10 +0800

    bugfix:修正sed命令导致无法启动问题

--- startup.sh
+++ startup.sh
@@ -8,8 +8,6 @@ if [ ! -f /subversion/config/subversion.conf ]; then
     mkdir -p /subversion/template/{trunk,branches,tags}
     svn import /subversion/template file:///repos/project_test -m "Initial commit"
 fi
-
-sed -i 's/#ServerName www.example.com:80/ServerName 0.0.0.0:80/g' /etc/httpd/conf/httpd.conf
     
 rm -rf /etc/httpd/conf.d/subversion.conf
 rm -rf /var/run/httpd/httpd.pid
