f7916d98eb82cd8ad7b159323b73056be09f1cbd
xiangjl <xjl-tommy@qq.com>
2017-08-14 23:46:51 +0800

    bugfix:某些情况下停止容器后无法再次启动

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ MAINTAINER XiangJL xjl-tommy@qq.com
 
 # install software
 RUN yum makecache fast && \
-    yum install -y subversion httpd mod_dav_svn && \
+    yum install -y supervisor subversion httpd mod_dav_svn && \
     yum clean all && \
     sed -i 's/#ServerName www.example.com:80/ServerName 0.0.0.0:80/g' /etc/httpd/conf/httpd.conf && \
     mkdir -p /subversion/config /subversion/logs /repos /docker
--- startup.sh
+++ startup.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 if [ ! -f /subversion/config/subversion.conf ]; then
-    cp /docker/subversion.conf /subversion/config/subversion.conf
-    cp /docker/authz.conf /subversion/config/authz.conf
-    htpasswd -bc /subversion/config/passwd.conf svn password
-    svnadmin create /repos/project_test
-    mkdir -p /subversion/template/{trunk,branches,tags}
-    svn import /subversion/template file:///repos/project_test -m "Initial commit"
+  cp /docker/subversion.conf /subversion/config/subversion.conf
+  cp /docker/authz.conf /subversion/config/authz.conf
+  htpasswd -bc /subversion/config/passwd.conf svn password
+  svnadmin create /repos/project_test
+  mkdir -p /subversion/template/{trunk,branches,tags}
+  svn import /subversion/template file:///repos/project_test -m "Initial commit"
 fi
     
-rm -rf /etc/httpd/conf.d/subversion.conf
-rm -rf /var/run/httpd/httpd.pid
+ln -sf /subversion/config/subversion.conf /etc/httpd/conf.d/subversion.conf
 
-cp /subversion/config/subversion.conf /etc/httpd/conf.d/subversion.conf
-
-/usr/sbin/httpd -D FOREGROUND
+if [ ! -f /run/httpd/httpd.pid ]; then
+  rm -rf /run/httpd/httpd.pid
+fi
+exec /usr/sbin/httpd -DFOREGROUND
